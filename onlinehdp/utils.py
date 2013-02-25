93306f2a24cd29476368ad7e85367478af338119
Flávio C. Coelho <fccoelho@gmail.com>
2013-02-25 10:01:45 -0300

    minor refactoring

--- onlinehdp/utils.py
+++ onlinehdp/utils.py
@@ -1,10 +1,14 @@
 #! /usr/bin/python
 
-''' several useful functions '''
+"""
+several useful functions
+"""
+
 import numpy as np
 
+
 def log_normalize(v):
-    ''' return log(sum(exp(v)))'''
+    """ return log(sum(exp(v)))"""
 
     log_max = 100.0
     if len(v.shape) == 1:
@@ -21,27 +25,30 @@ def log_normalize(v):
         log_norm = np.log(tot) - log_shift
         v = v - log_norm[:,np.newaxis]
 
-    return (v, log_norm)
+    return v, log_norm
+
 
 def log_sum(log_a, log_b):
-	''' we know log(a) and log(b), compute log(a+b) '''
-	v = 0.0;
-	if (log_a < log_b):
-		v = log_b+np.log(1 + np.exp(log_a-log_b))
-	else:
-		v = log_a+np.log(1 + np.exp(log_b-log_a))
-	return v
+    """ we know log(a) and log(b), compute log(a+b) """
+    v = 0.0;
+    if (log_a < log_b):
+        v = log_b+np.log(1 + np.exp(log_a-log_b))
+    else:
+        v = log_a+np.log(1 + np.exp(log_b-log_a))
+    return v
 
 
 def argmax(x):
-	''' find the index of maximum value '''
-	n = len(x)
-	val_max = x[0]
-	idx_max = 0
+    """ find the index of maximum value
+    :param x:
+    """
+    n = len(x)
+    val_max = x[0]
+    idx_max = 0
 
-	for i in range(1, n):
-		if x[i]>val_max:
-			val_max = x[i]
-			idx_max = i		
+    for i in range(1, n):
+        if x[i]>val_max:
+            val_max = x[i]
+            idx_max = i
 
-	return idx_max			
+    return idx_max
