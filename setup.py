92758d130b68313b706a9f1eef363bfab0ed2d1a
Flávio C. Coelho <fccoelho@gmail.com>
2013-03-01 15:14:20 -0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from Cython.Build import cythonize
 
 setup(
     name='Topics',
-    version="0.2.1",
+    version="0.2.2",
     author= 'Flávio C. Coelho',
     author_email= "fccoelho@gmail.com",
     license= "GPLv3",
     description="Set of tools for topic modeling and visualization",
-    ext_modules=cythonize("visualization/*.pyx"),
+    ext_modules=cythonize("Topics/visualization/*.pyx"),
     packages = ['Topics', 'Topics.onlineldavb', 'Topics.onlinehdp', 'Topics.visualization'],
 #    package_data={'tests': ['data/*']},
 )
