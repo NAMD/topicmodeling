1145ad635c570ae127bad11a94d63ef21773fc84
Flávio C. Coelho <fccoelho@gmail.com>
2013-03-01 08:03:15 -0300

    added sphinx documentation

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-
+#coding:utf8
 from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    name='word_cloud',
-    ext_modules=cythonize("*.pyx"),
+    name='Topics',
+    version="0.1.0",
+    author= 'Flávio C. Coelho',
+    author_email= "fccoelho@gmail.com",
+    license= "GPLv3",
+    description="Set of tools for topic modeling and visualization",
+    ext_modules=cythonize("visualization/*.pyx"),
     package_dir={'word_cloud': '.'}
 )
