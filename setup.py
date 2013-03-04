c1252397e5e514a2265d1c4bf6875850c2f83fed
Flávio C. Coelho <fccoelho@gmail.com>
2013-03-04 09:32:07 -0300

    changed setup.py to be based on setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #coding:utf8
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
     name='Topics',
     version="0.2.2",
-    author= 'Flávio C. Coelho',
-    author_email= "fccoelho@gmail.com",
-    license= "GPLv3",
+    author='Flávio C. Coelho',
+    author_email="fccoelho@gmail.com",
+    license="GPLv3",
+    url="https://github.com/NAMD/topicmodeling",
     description="Set of tools for topic modeling and visualization",
     ext_modules=cythonize("Topics/visualization/*.pyx"),
-    packages = ['Topics', 'Topics.onlineldavb', 'Topics.onlinehdp', 'Topics.visualization'],
-    requires= ['numpy', 'PIL', 'cython>=0.18'],
-#    package_data={'tests': ['data/*']},
+    packages=['Topics', 'Topics.onlineldavb', 'Topics.onlinehdp', 'Topics.visualization'],
+    install_requires=['numpy', 'PIL', 'cython>=0.18'],
+    #    package_data={'tests': ['data/*']},
 )
