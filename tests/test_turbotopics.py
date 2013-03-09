82409dddc27670c104a47e4f98020a0b2900d429
Flávio C. Coelho <fccoelho@gmail.com>
2013-03-09 12:38:35 -0300

    added test for nestedsigbigrams

--- tests/test_turbotopics.py
+++ tests/test_turbotopics.py
@@ -4,6 +4,8 @@
 import unittest
 import Topics.visualization.turbotopics as tt
 import regex
+from glob import glob
+import codecs
 
 
 class TestTurboTopics(unittest.TestCase):
@@ -12,3 +14,21 @@ class TestTurboTopics(unittest.TestCase):
         stext = tt.strip_text(text)
         self.assertEqual(regex.sub(ur"[^\p{L}\p{N} ]+", u"", text), u"Meu Cão é paraplégico")
         self.assertEqual(stext, u"Meu Cão é paraplégico".lower())
+
+    def test_nested_sig_bigram_returns_counts_object(self):
+        fns = glob('data/corpus_pt_BR/*.txt')
+        corpus = []
+        for fn in fns:
+            with codecs.open(fn, encoding='utf8') as f:
+                corpus.append(f.read())
+        def iter_gen():
+            for doc in corpus:
+                yield doc
+
+        def update_fun(count, doc):
+            count.update_counts(doc, root_filter=tt.stop_filter)
+
+        ### compute significant n-grams
+        lr = tt.LikelihoodRatio(pvalue=0.01, use_perm=False)
+        cnts = tt.nested_sig_bigrams(iter_gen, update_fun, lr, 5)
+        self.assertIsInstance(cnts, tt.Counts)
